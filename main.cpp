bbc540db9950e65b9a0636fa3d09a286daf3827e
raldone01 <raldone01@gmail.com>
2022-02-13 16:11:40 +0100

    Made the example actually do something.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,26 @@
 
 #include <coroutine>
 #include <boost/asio.hpp>
-#include <boost/asio/ip/tcp.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace asio = boost::asio;
 
-boost::asio::awaitable<void> echo(tcp::socket socket)
-{
-  try
-  {
-    char data[1024];
-    for (;;)
-    {
-      std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), boost::asio::use_awaitable);
-      co_await async_write(socket, boost::asio::buffer(data, n), boost::asio::use_awaitable);
+boost::asio::awaitable<void> myfun(asio::io_context & io, std::string msg, boost::posix_time::seconds secs) {
+  asio::deadline_timer t(io, secs);
+  try {
+    while (true) {
+      co_await t.async_wait(asio::use_awaitable);
+      t.expires_from_now(secs);
+      std::cout << "msg " << msg << " " << std::hash<std::thread::id>{}(std::this_thread::get_id()) << std::endl;
     }
-  }
-  catch (std::exception& e)
-  {
+  } catch (std::exception& e) {
     std::printf("echo Exception: %s\n", e.what());
   }
 }
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  boost::asio::io_context io;
+  boost::asio::co_spawn(io, myfun(io, "co1", boost::posix_time::seconds(5)), boost::asio::detached);
+  boost::asio::co_spawn(io, myfun(io, "co2", boost::posix_time::seconds(2)), boost::asio::detached);
+  std::cout << "Hello World! " << std::hash<std::thread::id>{}(std::this_thread::get_id()) << std::endl;
+  io.run();
   return 0;
 }
